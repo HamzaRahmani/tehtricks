926f708aa77434b9ae46dd4ad380cf61ef7451ad
HamzaRahmani <mrezio450@gmail.com>
2019-07-06 23:02:20 -0400

    initial commit

---
+++ src/hoc/aux-wrap.js
@@ -0,0 +1,14 @@
+const aux = (props) => props.children;
+//RANDOM 
+//RANDOM 
+//RANDOM 
+//RANDOM 
+//RANDOM 
+//RANDOM 
+//RANDOM 
+//RANDOM 
+//RANDOM 
+//RANDOM 
+//RANDOM 
+//RANDOM 
+export default aux;
